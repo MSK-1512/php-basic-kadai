193ff8ba15329749b07cd4bcc5b749d255c92ad7
MSK-1512 <168532885+MSK-1512@users.noreply.github.com>
2024-07-25 14:27:59 +0900

    【課題提出】連想配列を作って値とキーを出力しよう

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>課題：連想配列を作って値とキーを出力しよう</title>
+    </head>
+
+    <body>
+        <p>
+            <?php
+            $vegetable_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+            print_r($vegetable_data);
+            ?>
+        </p>
+    </body>
+    
+</html>
